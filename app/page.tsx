0d071110d12a653eb1301f0007db574da16055e4
Roman <roman.medvedtckiy@gmail.com>
2024-05-14 12:00:53 +0300

    fetching comics, update Item component

--- app/page.tsx
+++ app/page.tsx
@@ -1,79 +1,44 @@
 "use client";
 
-import Item from "@/components/Item/Item";
+import { Item } from "@/components/Item/Item";
 import ItemReverse from "@/components/ItemReverse/ItemReverse";
-import Image from "next/image";
 import md5 from "md5";
 import { use, useEffect, useState } from "react";
+import { Comics } from "@/types/types";
 
-const privateKey = "1234";
-const publicKey = "1234";
+const privateKey = process.env.NEXT_PUBLIC_PRIVATE_API_KEY;
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_API_KEY;
 
 export default function Home() {
-  const [picture, setPicture] = useState("");
-  const [resourceURI, setResourceURI] = useState("");
-  const [buttonClick, setButtonClick] = useState(false);
+  const [comics, setComics] = useState<Comics[]>([]);
   const timestamp = Date.now().toString();
 
-  const standard_fantastic = "/standard_fantastic";
-  //https://gateway.marvel.com:443/v1/public/characters/1011006?apikey=d43a485d04ded3bc2ba5d975b31edb53
   const hash = md5(timestamp + privateKey + publicKey);
-  const auth = `?ts=${timestamp}&apikey=d43a485d04ded3bc2ba5d975b31edb53&hash=${hash}`;
+  const auth = `ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
-        `https://gateway.marvel.com:443/v1/public/characters/1009718${auth}`
+        `https://gateway.marvel.com:443/v1/public/comics?orderBy=focDate&limit=5&${auth}`
       );
       const data = await response.json();
-      setPicture(
-        data.data.results[0].thumbnail.path +
-          standard_fantastic +
-          "." +
-          data.data.results[0].thumbnail.extension
-      );
-      setResourceURI(data.data.results[0].comics.items[0].resourceURI);
+      setComics(data.data.results);
     };
     fetchData();
-  }, [auth]);
-
-  useEffect(() => {
-    if (buttonClick) {
-      const fetchInfo = async () => {
-        const response = await fetch(resourceURI + auth);
-        const data = await response.json();
-        console.log(data);
-      };
-      fetchInfo();
-    }
-  }, [buttonClick]);
-
-  const handleGetInfo = () => {
-    setButtonClick(true);
-  };
+  }, []);
 
   return (
     <>
-      <button
-        onClick={handleGetInfo}
-        className="m-10 p-3 bg-orange-700 border-none"
-      >
-        Get Info
-      </button>
-      <Image width={250} height={250} alt="character photo" src={picture} />
       <div className="mt-20">
-        <h2 className="font-sans text-3xl font-semibold">Персонажи</h2>
-        <div className="flex gap-10 justify-center">
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
+        <h2 className="font-sans text-3xl font-semibold">Комиксы</h2>
+        <div className="flex gap-10 justify-center flex-wrap">
+          {comics.map((item) => {
+            return <Item key={item.id} comics={item} />;
+          })}
         </div>
       </div>
       <div className="mt-20">
-        <h2 className="font-sans text-3xl font-semibold">Комиксы</h2>
+        <h2 className="font-sans text-3xl font-semibold">Персонажи</h2>
         <div className="flex gap-10 justify-center">
           <ItemReverse />
           <ItemReverse />
--- components/Item/Item.tsx
+++ components/Item/Item.tsx
@@ -1,21 +1,28 @@
+import { Comics } from "@/types/types";
 import Image from "next/image";
+import { FC } from "react";
 
-export default function Item() {
+interface Props {
+  comics: Comics;
+}
+
+export const Item: FC<Props> = ({ comics }) => {
   return (
-    <div className="mt-20">
+    <div className="mt-20 max-w-56">
       <Image
         className="
-        mb-10 rounded-2xl transition-transform duration-200 -skew-y-6 hover:scale-110"
-        src="/cap.jpg"
-        width={200}
-        height={200}
-        alt="Picture of element"
+        mb-10 bg-center rounded-2xl transition-transform duration-200 -skew-y-6 hover:scale-110"
+        src={
+          comics.thumbnail.path +
+          "/portrait_uncanny" +
+          "." +
+          comics.thumbnail.extension
+        }
+        width={224}
+        height={224}
+        alt={comics.title}
       />
-      <h3 className="mb-5 text-xl">Name</h3>
-      <p>
-        Description Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Nemo, ab.
-      </p>
+      <p className="mb-5 text-xl">{comics.title}</p>
     </div>
   );
-}
+};
